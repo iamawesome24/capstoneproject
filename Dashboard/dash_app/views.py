5d52f521e6a2b1731f68b0cd8aa709fae24d1dcc
atb824 <74148169+atb824@users.noreply.github.com>
2021-07-03 09:18:56 +0530

    gif

--- Dashboard/dash_app/views.py
+++ Dashboard/dash_app/views.py
@@ -174,7 +174,7 @@ def mripredict(request):
     context['a'] = 'The Results for MRI Scans are'
     context['b'] = 'Coloured regions indicate abnormality'
     context['c'] = 'static/assets/img/mriout.gif'
-    context['d'] = 'Red - Necrotic and Non-enhancing tumor core (NCR) 0%'
+    context['d'] = 'Red - Necrotic and Non-enhancing tumor core (NCR) 7%'
     context['e'] = 'Green - Peritumoral Edema (ED) 9%'
     context['f'] = 'Blue - GD - Enhancing Tumor (ET) 8%'
 
@@ -196,9 +196,9 @@ def cetpredict(request):
     context['a'] = 'The Results for CET Scans are'
     context['b'] = 'Coloured regions indicate abnormality'
     context['c'] = 'static/assets/img/cetout.gif'
-    context['d'] = 'Red - Necrotic and Non-enhancing tumor core (NCR) 0%'
-    context['e'] = 'Green - Peritumoral Edema (ED) 12%'
-    context['f'] = 'Blue - GD - Enhancing Tumor (ET) 2%'
+    #context['d'] = 'Red - Necrotic and Non-enhancing tumor core (NCR) 0%'
+    context['e'] = 'Green - Liver 12%'
+    context['f'] = 'Blue - Lesion 2%'
 
 
     return render(request, 'results.html', context)
